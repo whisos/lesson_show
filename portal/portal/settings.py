8f2656181299a7fc90cbacd39963ae5e492f55bd
MaxMedvedenko <medvedenkomaksim143@gmail.com>
2024-05-10 22:37:14 +0300

    add styles and font family

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -127,4 +127,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL = "static/"
+
--- portal/portal/urls.py
+++ portal/portal/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from authentication.views import base_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', base_view, name='base'),
 ]
