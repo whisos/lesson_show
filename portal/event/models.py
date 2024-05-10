83e661de332083da65e910dd9fc121151e118103
Bikerski <vibikerski@gmail.com>
2024-05-10 18:47:56 +0200

    Rebase

--- portal/event/models.py
+++ portal/event/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=50)
+    discription = models.CharField(max_length=150)
+    time = models.DateTimeField()
+    date = models.DateField()
