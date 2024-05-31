57750348053f739f1fa68236b89c8fd1e20763e8
Kawarime <kaliak.miroslav@gmail.com>
2024-05-31 18:22:01 +0300

    Changes

--- forum/models.py
+++ forum/models.py
@@ -26,7 +26,7 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    media = models.FileField(upload_to='comments_media/',blank = True, null =True)
+    #media = models.FileField(upload_to='comments_media/',blank = True, null =True)
 
     def get_absolute_url(self):
         return self.post.get_absolute_url()
